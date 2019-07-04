fe621c1ab4767bcefe8dc4f2d4206283ec277186
Jack Davenport <romejanic@gmail.com>
2019-07-04 20:32:23 +1000

    Added uniforms to shader

--- bin/res/shaders/shader.fs
+++ bin/res/shaders/shader.fs
@@ -1,6 +1,7 @@
 #version 330 core
 
 out vec4 out_fragColor;
+in vec2 v_uv;
 
 // directives for shader developers to use
 #define __SHADERTOY_DESKTOP__
@@ -10,6 +11,23 @@ out vec4 out_fragColor;
 #define __FRAG_UV v_uv
 #define glFragColor out_fragColor
 
+// program defined directives
+%s
+
+// uniform variables
+uniform float iTime;
+uniform float iTimeDelta;
+uniform vec3 iResolution;
+uniform int iFrame;
+uniform float iChannelTime[4];
+uniform vec3 iChannelResolution[4];
+uniform vec4 iMouse;
+uniform vec4 iDate;
+uniform float iSampleRate;
+// samplers
+%s
+
+// mainImage() definition
 void mainImage(out vec4 fragColor, in vec2 fragCoord);
 
 /***************SHADER CODE GOES HERE****************/
